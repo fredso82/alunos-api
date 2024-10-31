039e0665c127d672358d868d478bc282fd2d0e7c
fredso82 <fredso82@gmail.com>
2024-10-31 20:00:34 -0300

    Criação das rotas

--- server.js
+++ server.js
@@ -1,11 +1,45 @@
 const express = require('express');
+const { create, update, remove, findAll, find } = require('./respositories/alunoRepository.js');
 
 const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.use(express.json());
+
+app.post('/alunos', (req, res) => {
+    const { nome, email, nomeCurso } = req.body;
+    const aluno = create({ nome, email, nomeCurso });
+    res.status(201).json(aluno);
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.get('/alunos', (req, res) => {
+    const alunos = findAll();
+    res.json(alunos);
+});
+
+app.get('/alunos/:id', (req, res) => {
+    const { id } = req.params;
+    const aluno = find(id);
+    if (aluno) {
+        res.json(aluno);
+    }
+    res.status(404);
+});
+
+
+app.put('/alunos/:id', (req, res) => {
+    const { id } = req.params;
+    const { nome, email, nomeCurso } = req.body;
+    const aluno = update(id, { nome, email, nomeCurso });
+    res.json(aluno);
+});
+
+app.delete('/alunos/:id', (req, res) => {
+    const { id } = req.params;
+    remove(id);
+    res.status(204).send();
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
 });
